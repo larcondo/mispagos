ec43d3b52f6bb9ec985f3c655e6b030ccd380e1f
larcondo <larcondo90@gmail.com>
2024-04-08 16:42:36 -0300

    Reestructurar app e instalar librerias de testing

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+// CORS: Cross Origin Resource Sharing
+const whiteList = [
+  'https://mispagos.onrender.com',
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+  'http://localhost:3000',
+  'https://mispagos.3.us-1.fl0.io',
+];
+const corsOptions = {
+  credentials: true,
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not Allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+// app.use(cors({credentials: true, origin: 'http://127.0.0.1:5173'}))
+// app.use(cors());   // sin opciones (permite todo)
+
+//middleware for cookies
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use(express.static('dist'));
+
+const MONGODB_URI = (process.env.NODE_ENV === 'test')
+  ? process.env.TEST_DATABASE_URI
+  : process.env.DATABASE_URI;
+
+// Database
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log('Conectado a la base de datos...'))
+  .catch( error => console.log(error));
+
+// routes
+app.use('/info', require('./routes/root'));
+app.use('/api/login', require('./routes/login'));
+app.use('/api/register', require('./routes/register'));
+app.use('/api/refresh', require('./routes/refresh'));
+app.use('/api/logout', require('./routes/logout'));
+app.use('/api/config', require('./routes/config'));
+app.use('/api/pagos', require('./routes/api/pagos'));
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', require('./routes/testing'));
+}
+
+app.all('*', (req, res) => {
+  res.status(404);
+  res.send({ message: 'Not Found' });
+});
+
+module.exports = app;
